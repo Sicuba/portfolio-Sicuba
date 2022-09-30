6fb06a5e95b9625cc95f61558608a06a1bee243c
josevaldo <josevaldosicuba@gmail.com>
2022-09-30 14:41:59 +0100

    card

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react';
+import Head from 'next/head';
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-         <NextUIProvider>
-      <Component {...pageProps} />
+      <NextUIProvider>
+        <Head>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
+        </Head>
+        <Component {...pageProps} />
+        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></Script>
           </NextUIProvider>
     </>)
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { Button } from '@nextui-org/react';
+import { Card } from '@nextui-org/react';
+import { Col, Row, Text } from "@nextui-org/react";
 export default function Home() {
   return (
     <div>
@@ -69,9 +71,189 @@ export default function Home() {
         <div className='bg'>
           <div className="projects-title">
               <h1>
-                Projectos Web
+                Projectos Web 
               </h1>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, impedit? Consectetur illum eaque blanditiis earum.</p>
+          </div>
+          <div >
+          </div> 
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                 <Card css={{ w: "100%", h: "400px" }}>
+    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+      <Col>
+        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+          New
+        </Text>
+        <Text h3 color="black">
+          Acme camera
+        </Text>
+      </Col>
+    </Card.Header>
+    <Card.Body css={{ p: 0 }}>
+      <Card.Image
+        src="https://nextui.org/images/card-example-6.jpeg"
+        width="100%"
+        height="100%"
+        objectFit="cover"
+        alt="Card example background"
+      />
+    </Card.Body>
+    <Card.Footer
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#ffffff66",
+        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
+      <Row>
+        <Col>
+          <Text color="#000" size={12}>
+            Available soon.
+          </Text>
+          <Text color="#000" size={12}>
+            Get notified.
+          </Text>
+        </Col>
+        <Col>
+          <Row justify="flex-end">
+            <Button flat auto rounded color="secondary">
+              <Text
+                css={{ color: "inherit" }}
+                size={12}
+                weight="bold"
+                transform="uppercase"
+              >
+                Notify Me
+              </Text>
+            </Button>
+          </Row>
+        </Col>
+      </Row>
+    </Card.Footer>
+  </Card>
+              </div>
+              <div className="col-md-4">
+                 <Card css={{ w: "100%", h: "400px" }}>
+    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+      <Col>
+        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+          New
+        </Text>
+        <Text h3 color="black">
+          Acme camera
+        </Text>
+      </Col>
+    </Card.Header>
+    <Card.Body css={{ p: 0 }}>
+      <Card.Image
+        src="https://nextui.org/images/card-example-6.jpeg"
+        width="100%"
+        height="100%"
+        objectFit="cover"
+        alt="Card example background"
+      />
+    </Card.Body>
+    <Card.Footer
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#ffffff66",
+        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
+      <Row>
+        <Col>
+          <Text color="#000" size={12}>
+            Available soon.
+          </Text>
+          <Text color="#000" size={12}>
+            Get notified.
+          </Text>
+        </Col>
+        <Col>
+          <Row justify="flex-end">
+            <Button flat auto rounded color="secondary">
+              <Text
+                css={{ color: "inherit" }}
+                size={12}
+                weight="bold"
+                transform="uppercase"
+              >
+                Notify Me
+              </Text>
+            </Button>
+          </Row>
+        </Col>
+      </Row>
+    </Card.Footer>
+  </Card>
+              </div>
+              <div className="col-md-4">
+                 <Card css={{ w: "100%", h: "400px" }}>
+    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+      <Col>
+        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+          New
+        </Text>
+        <Text h3 color="black">
+          Acme camera
+        </Text>
+      </Col>
+    </Card.Header>
+    <Card.Body css={{ p: 0 }}>
+      <Card.Image
+        src="https://nextui.org/images/card-example-6.jpeg"
+        width="100%"
+        height="100%"
+        objectFit="cover"
+        alt="Card example background"
+      />
+    </Card.Body>
+    <Card.Footer
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#ffffff66",
+        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
+      <Row>
+        <Col>
+          <Text color="#000" size={12}>
+            Available soon.
+          </Text>
+          <Text color="#000" size={12}>
+            Get notified.
+          </Text>
+        </Col>
+        <Col>
+          <Row justify="flex-end">
+            <Button flat auto rounded color="secondary">
+              <Text
+                css={{ color: "inherit" }}
+                size={12}
+                weight="bold"
+                transform="uppercase"
+              >
+                Notify Me
+              </Text>
+            </Button>
+          </Row>
+        </Col>
+      </Row>
+    </Card.Footer>
+  </Card>
+                </div>
+            </div>
           </div>
        </div>
         
